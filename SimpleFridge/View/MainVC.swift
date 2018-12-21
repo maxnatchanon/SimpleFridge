db69428f911ba6800178331126e5aa7826d95996
Natchanon A <natchanon.347@gmail.com>
2018-12-21 20:39:07 +0700

    Add new fridge button

--- SimpleFridge/View/MainVC.swift
+++ SimpleFridge/View/MainVC.swift
@@ -14,27 +14,21 @@ import RxCocoa
 class MainVC: UIViewController {
 
     @IBOutlet weak var fridgeTableView: UITableView!
+    @IBOutlet weak var addFridgeView: UIView!
     
-    private var fridgeList = [Fridge]()
+    private var fridgeList = [Fridge]() // TODO: Move this to VM
     private let disposeBag = DisposeBag()
     private let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Fridge")
-        do {
-            fridgeList = try context.fetch(request) as! [Fridge]
-        } catch {
-            print("Fetching at edit page failed")
-        }
-        
         setUpTableView()
     }
     
     private func setUpTableView() {
         // Set row height
         fridgeTableView.rowHeight = 80
+        addFridgeView.layer.cornerRadius = 10
         
         // Bind data source
         let observableFridgeList = Observable.just(fridgeList)
@@ -45,14 +39,22 @@ class MainVC: UIViewController {
             cell.selectionStyle = .none
         }.disposed(by: disposeBag)
         
-        fridgeTableView.rx.modelSelected(Fridge.self)
-            .subscribe(onNext: { (fridge) in
-                // TODO: Go to item screen with 'fridge'
-                
-                if let selectedRowIndexPath = self.fridgeTableView.indexPathForSelectedRow {
-                    self.fridgeTableView.deselectRow(at: selectedRowIndexPath, animated: true)
-                }
-            }).disposed(by: disposeBag)
+        // Select row action
+        fridgeTableView.rx.modelSelected(Fridge.self).subscribe(onNext: { (fridge) in
+            // TODO: Go to item screen with 'fridge'
+            
+            if let selectedRowIndexPath = self.fridgeTableView.indexPathForSelectedRow {
+                self.fridgeTableView.deselectRow(at: selectedRowIndexPath, animated: true)
+            }
+        }).disposed(by: disposeBag)
+        
+        // Add fridge button
+        let addFridgeTap = UITapGestureRecognizer(target: self, action: #selector(self.showAddFridgeScn))
+        addFridgeView.addGestureRecognizer(addFridgeTap)
+    }
+    
+    @objc private func showAddFridgeScn() {
+        // TODO: Show screen
     }
     
     @IBAction func settingBtnPressed(_ sender: Any) {
