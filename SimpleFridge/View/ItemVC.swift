779444a0f3152bf6b29d26d189a53311e1b30570
Natchanon A <natchanon.347@gmail.com>
2018-12-25 23:56:22 +0700

    Add item detail view

--- SimpleFridge/View/ItemVC.swift
+++ SimpleFridge/View/ItemVC.swift
@@ -16,10 +16,16 @@ class ItemVC: UIViewController {
     
     @IBOutlet weak var itemTableView: UITableView!
     @IBOutlet weak var emptyView: UIView!
+    @IBOutlet weak var detailView: UIView!
+    @IBOutlet weak var detailViewBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var selectedItemIcon: UIImageView!
+    @IBOutlet weak var selectedItemNameLbl: UILabel!
+    @IBOutlet weak var selectedItemExpireMsg: UILabel!
     
     private var itemVM: ItemVM!
     var fridge: Fridge!
     private let disposeBag = DisposeBag()
+    var showingDetailView = false
     
     
     // MARK:- Main function
@@ -28,19 +34,22 @@ class ItemVC: UIViewController {
         super.viewDidLoad()
         
         itemVM = ItemVM(withFridge: fridge)
-        setUpTableView()
+        setUpUI()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         itemVM.fetchItemData()
     }
     
-    func setUpTableView() {
+    func setUpUI() {
         itemTableView.rowHeight = 84
         bindEmptyView()
         bindTableView()
+        bindDetailView()
+        setUpDetailView()
+        lockDetailView(hidden: true)
     }
-
+    
     @IBAction func addBtnPressed(_ sender: Any) {
         performSegue(withIdentifier: SegueIdentifier.showAdd, sender: fridge)
     }
@@ -56,7 +65,7 @@ class ItemVC: UIViewController {
     
     /// Bind emptyView to itemList.count
     /// if there is no item, show emptyView
-    func bindEmptyView() {
+    private func bindEmptyView() {
         itemVM.itemList.asObservable().filter { (list) -> Bool in
             list.count == 0
             }.bind { (list) in
@@ -72,7 +81,7 @@ class ItemVC: UIViewController {
     
     /// Bind item table view data source to itemList in itemVM
     /// Add row select action, show item detail
-    func bindTableView() {
+    private func bindTableView() {
         itemVM.itemList.asObservable()
             .bind(to: itemTableView.rx.items(cellIdentifier: ItemCell.identifier, cellType: ItemCell.self)) {
                 (row, item, cell) in
@@ -80,9 +89,61 @@ class ItemVC: UIViewController {
                 cell.selectionStyle = .none
             }.disposed(by: disposeBag)
         
-        itemTableView.rx.modelSelected(Item.self).subscribe(onNext: { (item) in
-            // TODO: Show item detail
+        itemTableView.rx.itemSelected.subscribe(onNext: { [weak self] indexPath in
+            if (!(self!.showingDetailView)) {
+                self!.itemVM.selectItem(atIndexPath: indexPath)
+                self!.lockDetailView(hidden: false)
+            } else {
+                self!.lockDetailView(hidden: true)
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.3, execute: {
+                    self!.itemVM.selectItem(atIndexPath: indexPath)
+                    self!.lockDetailView(hidden: false)
+                })
+            }
+            
         }).disposed(by: disposeBag)
+    }
+    
+    /// Bind detail view with itemVM.selectedItem
+    private func bindDetailView() {
+        itemVM.selectedItem.asObservable().bind { (selectedItem) in
+                if (selectedItem != nil) {
+                    self.selectedItemNameLbl.text = selectedItem!.name!
+                    // TODO: Add all detail
+                }
+            }.disposed(by: disposeBag)
+    }
+    
+    /// Set up pan gesture recognizer for detail view
+    private func setUpDetailView() {
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(self.moveDetailView(_:)))
+        detailView.isUserInteractionEnabled = true
+        detailView.addGestureRecognizer(panGesture)
+    }
+    
+    @objc private func moveDetailView(_ sender: UIPanGestureRecognizer) {
+        switch sender.state {
+        case .began, .changed:
+            let translation = sender.translation(in: self.view)
+            var newConst = detailViewBottomConstraint.constant - translation.y
+            newConst = (newConst > 0) ? 0 : newConst
+            detailViewBottomConstraint.constant = newConst
+            sender.setTranslation(CGPoint.zero, in: self.view)
+        default:
+            if (detailViewBottomConstraint.constant > -120) {
+                lockDetailView(hidden: false)
+            } else {
+                lockDetailView(hidden: true)
+            }
+        }
+    }
+    
+    private func lockDetailView(hidden: Bool) {
+        UIView.animate(withDuration: 0.3, delay: 0.0, options: .curveEaseInOut, animations: {
+            self.detailViewBottomConstraint.constant = (hidden) ? -300 : 0
+            self.view.layoutIfNeeded()
+        })
+        showingDetailView = !hidden
     }
     
     /// Prepare for segue
